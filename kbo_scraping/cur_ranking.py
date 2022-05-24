f8e12ba9cbefa4f5d6d27069103b2b8864f32d94
camellia-ver <jakahi435@gmail.com>
2022-05-24 12:02:49 +0900

    순위 수집 완성

--- kbo_scraping/cur_ranking.py
+++ kbo_scraping/cur_ranking.py
@@ -1,33 +1,23 @@
 # KBO 순위 정보 수집
 # 시범 경기 외의 정규리그 순위만 수집
-# 2022 ~ ing는 4월2일 개막전 부터 시즌 종료일까지의 매일 순위 정보 수집
+# 현시점의 순위를 수집
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
-# 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
-
 import time
-import pyautogui
-import pyperclip
 
-# 브라우저 꺼짐 방지
-chrome_options = Options()
-chrome_options.add_experimental_option("detach",True)
-
-# 불필요한 에러 메시지 없에기
-chrome_options.add_experimental_option("excludeSwitches",["enable-logging"])
-
-# 창 숨기기
-chrome_options.add_argument("headless")
+options = Options()
+options.add_experimental_option("detach",True)
+options.add_experimental_option("excludeSwitches",["enable-logging"])
+options.add_argument("headless")
 
 service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service,options=chrome_options)
+driver = webdriver.Chrome(service=service,options=options)
 
-# 웹페이지 해당 주소 이동
-driver.implicitly_wait(5) # 웹페이지가 로딩 될때까지 5초는 기다림
-driver.maximize_window() # 화면 최대화
+driver.implicitly_wait(5)
 driver.get("https://www.koreabaseball.com/TeamRank/TeamRank.aspx")
+
+tbody = driver.find_element_by_xpath('//*[@id="cphContents_cphContents_cphContents_udpRecord"]/table/tbody').text
--- kbo_scraping/old_ranking.py
+++ kbo_scraping/old_ranking.py
@@ -1,33 +1,43 @@
 # KBO 순위 정보 수집
 # 시범 경기 외의 정규리그 순위만 수집
-# 1982 ~ 2021 동안의 순위는 최종 순위만 수집
+# 1982 ~ 2021의 최종순위
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
-# 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.support.ui import Select
+import requests
+from bs4 import BeautifulSoup
 import time
-import pyautogui
-import pyperclip
 
-# 브라우저 꺼짐 방지
-chrome_options = Options()
-chrome_options.add_experimental_option("detach",True)
+url = "https://www.koreabaseball.com/TeamRank/TeamRank.aspx"
 
-# 불필요한 에러 메시지 없에기
-chrome_options.add_experimental_option("excludeSwitches",["enable-logging"])
-
-# 창 숨기기
-chrome_options.add_argument("headless")
+options = Options()
+options.add_experimental_option("detach",True)
+options.add_experimental_option("excludeSwitches",["enable-logging"])
+# options.add_argument("headless")
 
 service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service,options=chrome_options)
+driver = webdriver.Chrome(service=service,options=options)
 
-# 웹페이지 해당 주소 이동
-driver.implicitly_wait(5) # 웹페이지가 로딩 될때까지 5초는 기다림
-driver.maximize_window() # 화면 최대화
-driver.get("https://www.koreabaseball.com/TeamRank/TeamRank.aspx")
+driver.implicitly_wait(5)
+driver.get(url)
+
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html,'html.parser')
+
+# 1999,2000 양대리그
+for year in range(1999,2000):
+    select = Select(driver.find_element_by_xpath('//*[@id="cphContents_cphContents_cphContents_ddlYear"]')).select_by_value(str(year))
+    time.sleep(1)
+    if year == 1999 or year == 2000:
+        # 드림리그
+        tbody1 = driver.find_element_by_xpath('//*[@id="cphContents_cphContents_cphContents_udpRecord"]/table/tbody').text
+        # 매직리그
+        tbody2 = driver.find_element_by_xpath('//*[@id="cphContents_cphContents_cphContents_pnlMagicLeague"]/table/tbody').text
+    else:
+        tbody = driver.find_element_by_xpath('//*[@id="cphContents_cphContents_cphContents_udpRecord"]/table/tbody').text
+    time.sleep(1)
