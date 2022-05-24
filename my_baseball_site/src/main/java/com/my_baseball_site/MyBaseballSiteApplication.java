a068a56c3b865b545d9b5627e286b24c201290cf
camellia-ver <jakahi435@gmail.com>
2022-05-24 12:09:22 +0900

    spring boot 생성

---
+++ my_baseball_site/src/main/java/com/my_baseball_site/MyBaseballSiteApplication.java
@@ -0,0 +1,13 @@
+package com.my_baseball_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseballSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBaseballSiteApplication.class, args);
+	}
+
+}
---
+++ my_baseball_site/src/test/java/com/my_baseball_site/MyBaseballSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_baseball_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBaseballSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
