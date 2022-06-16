9b6ca172fc8c9d274165c36db67f81ed257e212f
camellia-ver <jakahi435@gmail.com>
2022-06-16 23:43:29 +0900

    팀 기록 수집 기능 완성

---
+++ kbo_scraping/team_record/hitter/team_hitter_record_by_year.py
@@ -0,0 +1,74 @@
+# 2022 ~ yesterday 팀 기록 수집
+# 타자 기록
+
+from asyncio import constants
+from unittest import result
+from certifi import contents
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import pymysql
+from pymysql import cursors
+import time,datetime
+
+def getData(series,year):
+    result_data = []
+
+    contents = driver.find_elements(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/table/tbody/tr')
+
+    if len(contents) == 0:
+        return
+
+    for content in contents:
+        content = content.text.split(' ')
+        del content[0]
+        result_data.append(content)
+
+    next_btn = driver.find_element(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/div/div/a[2]')
+    next_btn.click()
+
+    time.sleep(5)
+
+    contents = driver.find_elements(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/table/tbody/tr')
+
+    for i,content in enumerate(contents):
+        content = content.text.split(' ')
+        del content[2]
+        del content[1]
+        del content[0]
+
+        result_data[i].extend(content)
+        result_data[i].extend(series)
+        result_data[i].extend(year)
+
+    for data in result_data:
+        sql_data = "'" + ",'".join(data) + "'"
+
+options = Options()
+options.add_experimental_option("detach",True)
+options.add_experimental_option("excludeSwitches",["enable-logging"])
+options.add_argument("headless")
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service,options=options)
+
+driver.implicitly_wait(5)
+driver.get("https://www.koreabaseball.com/Record/Team/Hitter/Basic1.aspx")
+
+# 2001년~2022년
+for year in range(2001,2023):
+    year = str(year)
+    select = Select(driver.find_element(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_ddlSeason_ddlSeason"]')).select_by_value(str(year))
+
+    time.sleep(5)
+    # data = getData("정규시즌",year)
+
+    select_series = {'와일드카드':4,'준플레이오프':3,'플레이오프':5,'한국시리즈':7}
+    
+    for series,i in select_series.items():
+        select = Select(driver.find_element(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_ddlSeries_ddlSeries"]')).select_by_value(str(i))
+        time.sleep(5)
+        getData(series,year)
---
+++ kbo_scraping/team_record/runner/team_runner_record_update.py
@@ -0,0 +1,64 @@
+# 올해 팀 기록 업데이트
+# 타자 기록
+
+from unittest import result
+from certifi import contents
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import pymysql
+from pymysql import cursors
+import time,datetime
+
+def getData(series,year):
+    result_data = []
+
+    contents = driver.find_elements(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/table/tbody/tr')
+
+    if len(contents) == 0:
+        return
+
+    for content in contents:
+        content = content.text.split(' ')
+        del content[0]
+        result_data.append(content)
+
+    next_btn = driver.find_element(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/div/div/a[2]')
+    next_btn.click()
+
+    time.sleep(5)
+
+    contents = driver.find_elements(by=By.XPATH,value='//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[2]/table/tbody/tr')
+
+    for i,content in enumerate(contents):
+        content = content.text.split(' ')
+        del content[2]
+        del content[1]
+        del content[0]
+
+        result_data[i].extend(content)
+
+    result_data.append(series)
+    result_data.append(year)
+
+    sql_data = "'" + ",'".join(result_data) + "'"
+
+options = Options()
+options.add_experimental_option("detach",True)
+options.add_experimental_option("excludeSwitches",["enable-logging"])
+options.add_argument("headless")
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service,options=options)
+
+driver.implicitly_wait(5)
+driver.get("https://www.koreabaseball.com/Record/Team/Runner/Basic.aspx")
+
+now = datetime.datetime.now()
+cur_year = now.year
+
+time.sleep(5)
+data = getData("정규시즌",cur_year)
